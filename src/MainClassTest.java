612a8a53ca780fbf9c706ef7d1ad3f2d944f6e08
Lunna3000 <polin.ka93@list.ru>
2018-10-21 16:22:42 +0300

    Second task

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -11,6 +11,17 @@ public class MainClassTest extends MainClass {
         } else {
             System.out.println("No, number != 14");
         }
-
     }
+
+    @Test
+    public void testGetClassNumber(){
+        int b = this.getClassNumber();
+
+        if (b > 45) {
+            System.out.println("Yes, number > 45");
+        } else {
+            System.out.println("No, number < 45 or number = 45");
+        }
+    }
+
 }

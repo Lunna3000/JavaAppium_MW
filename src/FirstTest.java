dc76aec9a6799208d85201e13d404932ee1ba84e
Lunna3000 <polin.ka93@list.ru>
2018-10-24 00:12:29 +0300

    Ex2

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,16 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -34,8 +41,42 @@ public class FirstTest {
     }
 
     @Test
-    public void firstTest()
+    public void Ex2()
     {
-        System.out.println("First test run");
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannon find Search Wikipedia input",
+                5
+        );
+
+        WebElement search_string = waitForElementPresent(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Cannon find Search Wikipedia input",
+                5
+        );
+
+        String text_in_line = search_string.getAttribute("text");
+
+        Assert.assertEquals(
+                "No",
+                "Search…",
+                text_in_line
+        );
+    }
+
+    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementAndClick(By by, String error_message, long timeoutInSeconds)
+    {
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.click();
+        return element;
     }
 }
